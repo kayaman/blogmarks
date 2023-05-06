aac3da9437db414055a7f49318ea6241372e691d
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-05-06 18:08:17 -0300

    sanity's sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,14 +3,27 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  projectId: 'zyofsvoi',
-  dataset: 'production',
-  name: 'bookmarks',
-  plugins: [deskTool(), visionTool()],
-  title: 'bookmarks',
-  apiVersion: '2023-05-06',
-  schema: {
-    types: schemaTypes,
+export default defineConfig([
+  {
+    projectId: 'zyofsvoi',
+    dataset: 'production',
+    name: 'bookmarks-prod',
+    basePath: '/admin',
+    title: 'Production',
+    plugins: [deskTool(), visionTool()],
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    projectId: 'zyofsvoi',
+    dataset: 'development',
+    name: 'bookmarks-dev',
+    basePath: '/admin',
+    title: 'Development',
+    plugins: [deskTool(), visionTool()],
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
