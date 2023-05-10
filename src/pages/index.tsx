fd56a930217d9beb46caec711b38ee9396837699
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-05-10 11:24:38 -0300

    sorting desc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import {createClient} from 'next-sanity'
 import clientConfig from '@/sanity/clientConfig'
 import BookmarksLayout from '@/layouts/BookmarksLayout'
+import siteMetadata from '@/data/siteMetadata'
 
 export default function Home({bookmarks, title}) {
   return (
@@ -15,11 +16,11 @@ const client = createClient(clientConfig)
 export async function getStaticProps() {
   const bookmarks = await client.fetch(
     `*[_type == 'bookmark' && private != true]
-    {_id, link, title, _createdAt, 'tags': tags[]->{_id, name}}
-    | order(_createdAt desc)
+    {_id, link, title, _createdAt, _updatedAt, 'tags': tags[]->{_id, name}}
+    | order(_updatedAt desc)
     `
   )
-  const title = 'Home'
+  const title = siteMetadata.homePageTitle
   return {
     props: {
       bookmarks,
