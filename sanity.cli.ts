acdcbedc2b26708ceca7bce3bbe5d2a697dbf778
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-05-10 12:48:05 -0300

    turning prod to dev (#27)

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'zyofsvoi',
-    dataset: 'production',
+    dataset: 'development',
   },
 })
--- schemas/Tag.ts
+++ schemas/Tag.ts
@@ -1,4 +1,4 @@
-import {defineType, defineField, defineArrayMember} from 'sanity'
+import {defineType, defineField} from 'sanity'
 
 const tag = defineType({
   name: 'tag',
