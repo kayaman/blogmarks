c0ba0de30f301e46367dd2e2a39eb2193e29edba
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-05-12 17:11:33 -0300

    fix date created (#35)

---
+++ src/components/CreatedAt.tsx
@@ -0,0 +1,25 @@
+import siteMetadata from '@/data/siteMetadata'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+dayjs.extend(relativeTime)
+dayjs.extend(customParseFormat)
+
+export const CreatedAt = ({date}) => {
+  const daysAgo = new Date().getDate() - 1
+  let formattedDate = ''
+
+  if (dayjs().isAfter(dayjs(daysAgo))) {
+    formattedDate = dayjs(date).fromNow()
+  } else {
+    formattedDate = dayjs(date).format('MMMM D, YYYY')
+  }
+  return (
+    <dl>
+      <dt className="sr-only">Created on</dt>
+      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+        <time dateTime={String(date)}>{formattedDate}</time>
+      </dd>
+    </dl>
+  )
+}
