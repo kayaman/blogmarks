a9b9fff3fcb319a4bbfef8f0acea13b56f7be3df
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-05-13 23:43:08 -0300

    fixes

--- src/pages/references.tsx
+++ src/pages/references.tsx
@@ -1,19 +1,13 @@
 import BookmarksLayout from '@/layouts/BookmarksLayout'
-import Bookmark from '@/types/Bookmark'
 import siteMetadata from '@/data/siteMetadata'
 import {getAllBookmarksByTagName} from '@/server/persistence/sanityRepository'
 
-interface IRerencesProps {
-  bookmarks: Bookmark[]
-  title: string
-}
-
-const References = ({bookmarks, title}: IRerencesProps) => {
+const References = ({bookmarks, title}) => {
   return <BookmarksLayout bookmarks={bookmarks} title={title} />
 }
 
 export async function getStaticProps() {
-  const title = siteMetadata.toolsPageTitle
+  const title = siteMetadata.referencesPageTitle
   const tagName = 'reference'
   const bookmarks = await getAllBookmarksByTagName(tagName)
 
