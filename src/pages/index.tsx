c34688b03f115017338e1fb9bfec9ee85a39f1aa
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-05-28 19:25:28 -0300

    UI lookimh beauiful (#59)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox } from 'react-instantsearch-hooks-web';
 import siteMetadata from '@/data/siteMetadata'
 import {getAllBookmarksCount, getAllBookmarksPaginated} from '@/server/persistence/sanityRepository'
 import BookmarksSearchLayout from '@/layouts/BookmarksSearchLayout'
 
 const PAGE_SIZE = siteMetadata.pageSize
 
+const searchClient = algoliasearch('IUBI46TDU9', "934609271f37b4520c961634b5f9b592")
+
 export default function Home({bookmarks, title, pagination}) {
+  <InstantSearch searchClient={searchClient} indexName='bookmarksIndex'>
+    <SearchBox  />
+  </InstantSearch>
+
+
   return <BookmarksSearchLayout bookmarks={bookmarks} title={title} pagination={pagination} />
 }
 
