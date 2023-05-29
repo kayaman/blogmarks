614cd203bd1ac1f95e19adca26943aa5bf7e5902
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-05-28 22:41:49 -0300

    kidding wth algolia

---
+++ src/pages/algolia.tsx
@@ -0,0 +1,21 @@
+import algoliasearch from "algoliasearch"
+import { Hits, InstantSearch, SearchBox } from "react-instantsearch-hooks-web"
+
+
+const hitsHandler = ({hit}) => {
+    console.log(hit)
+    return <div>{hit.title}</div>
+}
+const searchClient = algoliasearch('IUBI46TDU9',"934609271f37b4520c961634b5f9b592")
+
+const AlgoliaSeach = () => {
+
+
+    return (
+        <InstantSearch searchClient={searchClient} indexName='bookmarksIndex'>
+            <SearchBox  />
+            <Hits hitComponent={hitsHandler} /> 
+        </InstantSearch>
+    )
+}
+export default AlgoliaSeach
