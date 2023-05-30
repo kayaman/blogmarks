67c9678409f25a6f1af0cee38e8728152ab4e1fd
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-05-30 18:09:50 -0300

    leaving out a lot of mess

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,29 +4,28 @@ import siteMetadata from '@/data/siteMetadata'
 import {getAllBookmarksCount, getAllBookmarksPaginated} from '@/server/persistence/sanityRepository'
 import BookmarksSearchLayout from '@/layouts/BookmarksSearchLayout'
 import BookmarkCard from '@/components/BookmarkCard'
-import Bookmark from 'schemas/Bookmark'
 
 const PAGE_SIZE = siteMetadata.pageSize
 
 const searchClient = algoliasearch('IUBI46TDU9', '934609271f37b4520c961634b5f9b592')
 
-const hitHandler = (hit): BookmarkCard => {
+const hitHandler = (hit) => {
   return <BookmarkCard bookmark={hit} />
 }
 
 export default function Home({bookmarks, title, pagination}) {
-  return (
-    // <>
-    //   <InstantSearch searchClient={searchClient} indexName="bookmarksIndex">
-    //     <SearchBox />
-    //     <Hits hitComponent={hitHandler} />
-    //   </InstantSearch>
-    <BookmarksSearchLayout
-      bookmarks={bookmarks}
-      title={title}
-      pagination={pagination}
-    ></BookmarksSearchLayout>
-  )
+  if (pagination['currentPage'] > 1) {
+    return <h1>asdfgh</h1>
+  } else {
+    return (
+      <>
+        <InstantSearch searchClient={searchClient} indexName="bookmarksIndexProd">
+          <SearchBox />
+          <Hits hitComponent={hitHandler} />
+        </InstantSearch>
+      </>
+    )
+  }
 }
 // return <BookmarksSearchLayout bookmarks={bookmarks} title={title} pagination={pagination}>
 
