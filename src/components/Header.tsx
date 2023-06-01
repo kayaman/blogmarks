ecc06125a1180a002258239060f3c4187af43c4e
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-06-01 19:24:59 -0300

    moar BIG fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,22 @@ import Link from 'next/link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import Logo from './Logo'
+import algoliasearch from 'algoliasearch'
+import {Hits, InstantSearch} from 'react-instantsearch-hooks-web'
+import {SearchBox} from 'react-instantsearch-hooks-web'
+
+export const hitsHandler = ({hit}) => {
+  console.log(hit)
+  return <div>{hit.title}</div>
+}
+const searchClient = algoliasearch('IUBI46TDU9', '934609271f37b4520c961634b5f9b592')
 
 const Header = () => {
   return (
-    <header className="justify-left flex items-center pb-5 pl-5 pr-5 pt-6">
+    <header className="flex items-start pt-6 pb-5 pl-5 pr-5 justify-left">
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="justify-left flex items-center">
+          <div className="flex items-center justify-left">
             <div className="mr-3">
               <Logo />
             </div>
@@ -20,23 +29,32 @@ const Header = () => {
             <div className="h-5 text-xl font-semibold sm:block">{siteMetadata.pageTitle}</div>
           </div>
         </Link>
-        <p className="leading-2 text-base text-gray-300 dark:text-gray-400">
+        <p className="text-base text-gray-300 leading-2 dark:text-gray-400">
           {siteMetadata.description}
         </p>
       </div>
-      <div className="leading-2 flex items-end text-right align-top text-base">
-        <div className="text-lg">
-          {headerNavLinks.map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="p-1 text-lg text-gray-900 dark:text-gray-100 sm:p-4"
-            >
-              {link.title}
-            </Link>
-          ))}
+
+      <div className="flex flex-col flex-nowrap">
+        <div className="top-0 flex items-start text-base text-right align-top flex-nowrap justify-self-start leading-2">
+          <div className="text-lg">
+            {headerNavLinks.map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="p-1 text-lg text-gray-900 dark:text-gray-100 sm:p-4"
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+          <ThemeSwitch />
+          <div>
+            <InstantSearch searchClient={searchClient} indexName="bookmarksIndexProd">
+              <SearchBox />
+              <Hits hitComponent={hitsHandler} />
+            </InstantSearch>
+          </div>
         </div>
-        <ThemeSwitch />
         <MobileNav />
       </div>
     </header>
