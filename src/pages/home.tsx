b3a28bbed9f2f61cccaf59fd4611f1e542c711c4
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-06-23 18:02:24 -0300

    commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,37 @@
+import siteMetadata from '@/data/siteMetadata'
+import {
+  getAllBookmarksCount,
+  getAllBookmarksPaginated,
+} from '../server/persistence/sanityRepository'
+import BookmarksSearchLayout from '../layouts/BookmarksSearchLayout'
+
+export default async function Home({bookmarks, title, pagination, allbookmarks}) {
+  return (
+    <BookmarksSearchLayout
+      bookmarks={bookmarks}
+      title={title}
+      pagination={pagination}
+      allbookmarks={allbookmarks}
+    />
+  )
+}
+
+export const getServerSideProps = async () => {
+  const allbookmarks = (await getAllBookmarksPaginated(0, 1000)) || []
+  const bookmarks = (await getAllBookmarksPaginated(0, 25)) || []
+  const total = await getAllBookmarksCount()
+  const title = siteMetadata.homePageTitle
+  const pagination = {
+    currentPage: 1,
+    totalPages: Math.floor(total / siteMetadata.PAGE_SIZE),
+  }
+
+  return {
+    props: {
+      bookmarks,
+      title,
+      pagination,
+      allbookmarks,
+    },
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
-import algoliasearch from 'algoliasearch/lite'
-import {Hits, InstantSearch, SearchBox} from 'react-instantsearch-hooks-web'
 import siteMetadata from '@/data/siteMetadata'
 import {getAllBookmarksCount, getAllBookmarksPaginated} from '@/server/persistence/sanityRepository'
 import BookmarksSearchLayout from '@/layouts/BookmarksSearchLayout'
-import BookmarkCard from '@/components/BookmarkCard'
-import {useEffect, useState, useRef} from 'react'
+import {useState} from 'react'
 
 const PAGE_SIZE = siteMetadata.pageSize
-
-const searchClient = algoliasearch('IUBI46TDU9', '934609271f37b4520c961634b5f9b592')
-
-const hitHandler = (hit) => {
-  return <BookmarkCard bookmark={hit} />
-}
 
 export default function Home({bookmarks, title, pagination}) {
   const [isLoading, setIsLoading] = useState(false)
@@ -25,10 +16,6 @@ export default function Home({bookmarks, title, pagination}) {
       {!isLoading && bookmarks && (
         <BookmarksSearchLayout bookmarks={bookmarks} title={title} pagination={pagination} />
       )}
-      <InstantSearch searchClient={searchClient} indexName="bookmarksIndexProd">
-        <SearchBox />
-        <Hits hitComponent={hitHandler} />
-      </InstantSearch>
     </>
   )
 }
