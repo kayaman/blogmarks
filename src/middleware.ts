a78a06f65c55719316e46fd6fbceb1d8511ce4c0
Marco Antonio Gonzalez Junior <m@rco.sh>
2024-05-19 12:58:45 -0300

    redirect to admin

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import {NextResponse} from 'next/server'
+import type {NextRequest} from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  console.log('middleware, url: ', request.url)
+  if (request.url === 'https://admin.blogmarks.dev') {
+    return NextResponse.redirect('https://blogmarks.dev/admin')
+  }
+  return NextResponse.next()
+}
