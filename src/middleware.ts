2a8651eb7b8044615785efd501ee358b49a0e1b3
Marco Antonio Gonzalez Junior <m@rco.sh>
2024-06-01 10:56:21 -0300

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import {NextResponse} from 'next/server'
 import type {NextRequest} from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  if (request.url === 'https://admin.blogmarks.dev/') {
+  if (request.url.startsWith('https://admin.blogmarks.dev')) {
     return NextResponse.redirect('https://blogmarks.dev/admin')
   }
   return NextResponse.next()
